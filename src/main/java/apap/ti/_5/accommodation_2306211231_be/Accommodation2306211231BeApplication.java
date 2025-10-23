4c83834e2de03e19007dbccf52739eadcf9f1b73
Rafansya Daryltama Santoso <rafansya.daryltama@ui.ac.id>
2025-10-23 12:53:36 +0700

    feat-0: initialise project

---
+++ src/main/java/apap/ti/_5/accommodation_2306211231_be/Accommodation2306211231BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.accommodation_2306211231_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accommodation2306211231BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accommodation2306211231BeApplication.class, args);
+	}
+
+}
